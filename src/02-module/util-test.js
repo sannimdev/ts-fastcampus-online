9f83d0a33df06d5ef434489700d9c5f1694a3376
sannimdev <inegg.apps@gmail.com>
2021-03-29 21:16:04 +0900

    3. 모듈 불러오기와 Common.js 방식으로 사용하는 옵션 지정하여 컴파일하기

---
+++ src/02-module/util-test.js
@@ -0,0 +1,6 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+/* 🤗 ES6로 컴파일하였지만 common.js 모듈 방식으로 구문이 작성된 것을 확인할 수 있다. */
+const util_1 = require('./util');
+const value = util_1.default(1, 2);
+console.log(value);
