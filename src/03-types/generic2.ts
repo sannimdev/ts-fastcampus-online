2b8e64d0f2ee04e2620136d03f0c7756df93a037
sannimdev <inegg.apps@gmail.com>
2021-04-08 20:29:49 +0900

    12-3. 제네릭(3)

--- src/03-types/generic2.ts
+++ src/03-types/generic2.ts
@@ -1,8 +1,28 @@
-class LocalDB<T> {
+interface DB<T> {
+    add(v: T): void;
+    get(): T;
+}
+
+class D<T> implements DB<T> {
+    add(v: T): void {
+        throw new Error('Method not implemented.');
+    }
+    get(): T {
+        throw new Error('Method not implemented.');
+    }
+}
+
+interface JSONSerializer {
+    serialize(): string;
+}
+
+// T는 JSONSerializer의 하위 타입이다.
+class LocalDB<T extends JSONSerializer> implements DB<T> {
     constructor(private localStorageKey: string) {}
 
     add(v: T) {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(v));
+        // T라는 제네릭 타입은 또 JSONSerializer를 상속받고 있으므로 serialize라는 메서드를 갖고 있음을 보장함.
+        localStorage.setItem(this.localStorageKey, JSON.stringify(v.serialize()));
     }
 
     get(): T {
@@ -11,12 +31,24 @@ class LocalDB<T> {
     }
 }
 
-interface User {
-    name: string;
+interface Vegetable {
+    v: string;
 }
 
-const userDb = new LocalDB<User>('user');
-userDb.add({ name: 'jay' });
-// userDb.add('hello'); // 'string' 형식의 인수는 'User' 형식의 매개 변수에 할당될 수 없습니다.ts(2345)
-const userA = userDb.get();
-userA.name;
+interface Meat {
+    m: string;
+}
+
+interface Cart2<T> {
+    // T는 무조건 Vegitable이나 Meat의 하위 타입이어야 함. (ex: string은 T의 타입으로 쓰일 수 없음)
+    getItem(): T extends Vegetable ? Vegetable : Meat;
+}
+const cart1: Cart2<Vegetable> = {
+    getItem() {
+        return {
+            v: '',
+        };
+    },
+};
+
+cart1.getItem;
