13d54c1fbc083b09529e17b359957d6bd600f4ec
sannimdev <inegg.apps@gmail.com>
2021-04-13 05:35:00 +0900

    15-1. Catch the lion 실제 프로젝트 만들기
    - 환경 구축하고 실습환경 만들기
    - bootstrap 설치하여 ts에서 로드하기 등

---
+++ src/catch-the-lion-ts/src/Game.ts
@@ -0,0 +1,6 @@
+export class Game {}
+
+new Game();
+
+const version = 'v1';
+export default version;
---
+++ src/catch-the-lion-ts/src/index.ts
@@ -0,0 +1,4 @@
+import { Game } from './Game';
+import 'bootstrap/dist/css/bootstrap.css'; // CSS를 스크립트상 로드할 수 있음 (feat. parcel)
+
+new Game();
